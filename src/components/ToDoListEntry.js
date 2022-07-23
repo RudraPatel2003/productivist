3780836097f477306c0e35457897be628cf2ad6d
Rudra Patel <patelrudra2003@gmail.com>
2022-07-22 23:17:05 -0500

    Fixes Safari drop-shadow bug

--- src/components/ToDoListEntry.js
+++ src/components/ToDoListEntry.js
@@ -6,9 +6,12 @@ import X from "../assets/icons/X.svg"
 export default function ToDoListEntry(props) {
 
   return (
+    // drop shadows remain even after element is gone on Safari
+    // will-change-[filter] fixes that
+    // https://stackoverflow.com/questions/56478925/safari-drop-shadow-filter-remains-visible-even-with-hidden-element
     <>
     {props.isComplete ? 
-    <div className="flex flex-wrap flex-col w-[min(80%,600px)] h-[max(8rem, max-content)] bg-grey-pink text-grey p-4 mx-auto mb-8 rounded-2xl drop-shadow-normal break-words transition-all ease-in-out duration-250">
+    <div className="flex flex-wrap flex-col w-[min(80%,600px)] h-[max(8rem, max-content)] bg-grey-pink text-grey p-4 mx-auto mb-8 rounded-2xl drop-shadow-normal break-words transition-all ease-in-out duration-250 will-change-[filter] ">
       <p className="mb-auto w-full">{props.taskDescription}</p>
       <div className="flex flex-wrap gap-2 justify-end">
         <X onClick={() => props.handleToggleComplete(props.id)} className="cursor-pointer"/>
@@ -16,7 +19,7 @@ export default function ToDoListEntry(props) {
       </div>
     </div>
     :
-    <div className="flex flex-wrap flex-col w-[min(80%,600px)] h-[max(8rem, max-content)] bg-pink text-dark-maroon p-4 mx-auto mb-8 rounded-2xl drop-shadow-normal break-words transition-all ease-in-out duration-250">
+    <div className="flex flex-wrap flex-col w-[min(80%,600px)] h-[max(8rem, max-content)] bg-pink text-dark-maroon p-4 mx-auto mb-8 rounded-2xl drop-shadow-normal break-words transition-all ease-in-out duration-250 will-change-[filter]">
     <p className="mb-auto w-full">{props.taskDescription}</p>
       <div className="flex flex-wrap gap-2 justify-end">
         <Checkmark onClick={() => props.handleToggleComplete(props.id)} className="cursor-pointer"/>

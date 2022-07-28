578071dd3d627832d40a9d110234024f7657222b
Rudra Patel <patelrudra2003@gmail.com>
2022-07-27 21:40:21 -0500

    Adds GitHub and Google authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useSession } from "next-auth/react";
 
 export default function Header() {
+  const { data: session } = useSession();
+
   return (
     <header>
-      <p className="text-dark-maroon text-center text-2xl sm:text-3xl md:text-4xl font-[Courgette] py-5 ">productivist</p>
+      <p className="text-dark-maroon text-center text-2xl sm:text-3xl md:text-4xl font-[Courgette] py-5 ">
+        productivist
+      </p>
+      {session ? (
+        <p className="text-center text-dark-maroon mb-4">
+          Welcome, {session.user.name.split(" ")[0]}
+        </p>
+      ) : null}
     </header>
-  )
+  );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
-export { default as Header } from "./Header"
-export { default as ToDoList } from "./ToDoList"
-export { default as Footer } from "./Footer"
+export { default as Header } from "./Header";
+export { default as ToDoList } from "./ToDoList";
+export { default as SignInButton } from "./SignInButton";
+export { default as SignOutButton } from "./SignOutButton";
+export { default as Footer } from "./Footer";
